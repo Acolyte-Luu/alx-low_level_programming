83f836401ca377246338b8f0e92b78389d9de938
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-08 15:48:57 -0800

    using write to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-int q;
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
 }
