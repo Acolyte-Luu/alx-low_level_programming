dcb4d56fe1b7d94564a24a9c3d52870b7ce64c45
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-08 15:47:37 -0800

    printing with write in c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 /**
 *main - Entry point
 *char str1 - string to print
-*fwrite - print string to stdout
+*write - print string to stdout
 *Return: Always 0 (Success)
 */
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 int q;
-q = write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
 }
