002ea69774c5eac026f7e41a6d09310d0caa005e
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-08 15:52:57 -0800

    using write to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,15 @@
 #include <string.h>
 /**
 *main - Entry point
-*char str1 - string to print
+*filedes - placeholder of characters
+*buf - buffer stream to print
+*nbyte - number of bytes to print
 *write - print string to stdout
 *Return: Always 0 (Success)
 */
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
