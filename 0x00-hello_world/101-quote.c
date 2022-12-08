4db52620d46efbad229f7a4261eada44c0886d01
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-08 14:56:30 -0800

    write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 int q;
-Q = write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 60);
+q = write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
 }
