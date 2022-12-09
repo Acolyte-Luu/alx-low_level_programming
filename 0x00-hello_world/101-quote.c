ede3fc6e573639f3aefac1e671931fe1926b36a8
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-08 16:18:32 -0800

    write function print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <string.h>
 /**
 *main - Entry point
-*filedes - placeholder of characters
+*write - function to print
+*int filedes - placeholder of characters
 *buf - buffer stream to print
 *nbyte - number of bytes to print
-*write - print string to stdout
 *Return: Always 0 (Success)
 */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
