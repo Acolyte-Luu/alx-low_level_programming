f1e1530a5dd1aa83c9b50bc100a44b6885be401e
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 18:03:17 -0800

    print alphabets with do while

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*int ch - integer ch
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+do {
+putchar(ch);
+} while (ch != 'e' && ch != 'q');
+putchar('\n');
+return (0);
+}
