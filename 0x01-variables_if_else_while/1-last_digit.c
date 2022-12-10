343548fb6be211d045984cee86e80a5475efa89d
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 17:07:06 -0800

    if else statement with multiple conditions

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+/**
+*main - entry point
+*srand - random number generator
+*n - random integer
+*last_digit - last digit of int n
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n, last_digit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
+if (last_digit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+}
+else if (last_digit == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
+}
+else if (last_digit > 0 && last_digit < 6)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+}
+return (0);
+}
