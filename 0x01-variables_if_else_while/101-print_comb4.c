2fe6dcac7b8ecbf2f718ccebb382d2cb3a77c64f
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 19:59:10 -0800

    3-digit combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 int dig1, dig2, dig3, k = 0;
 for (dig1 = '0'; dig1 <= '9'; dig1++)
 {
-for (dig2 = dig1 + 1; dig2 <= '9'; dig2++)
+for (dig2 = dig1 + 1; dig2 <= '9'; dig2++, k++)
 {
-for (dig3 = dig2 + 1; dig3 <= '9'; dig3++, k++)
+for (dig3 = dig2 + 2; dig3 <= '9'; dig3++, k++)
 {
 if (k > 0)
 {
