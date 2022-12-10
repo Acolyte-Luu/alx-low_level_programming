a2e82cca7b78abeddb638fea3993835bea84126b
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 18:31:59 -0800

    printing digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*int ch - integer ch
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
