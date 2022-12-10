c3553ad0781d9571c4d4bc00ff86334311c41d92
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 19:11:47 -0800

    combo of digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,20 @@
 /**
 *main - Entry point
 *int ch - integer ch
+*k - checker integer
 *Return: Always 0 (Success)
 */
 int main(void)
 {
-int ch;
+int ch, k = 0;
 for (ch = '0'; ch <= '9'; ch++)
 {
 putchar(ch);
+if (k > 0)
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
