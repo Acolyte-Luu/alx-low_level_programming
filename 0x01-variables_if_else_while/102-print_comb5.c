b548965f0d172b9b4901b8fdaee85c827dc40b76
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 21:39:35 -0800

    Complex

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 int dig1, dig2, dig3, dig4, j = 0, k = 0;
 for (dig1 = '0'; dig1 <= '9'; dig1++, k++)
 {
-for (dig2 = '0'; dig2 <= '9'; dig2++)
+for (dig2 = '0'; dig2 <= '9'; dig2++, k++)
 {
 for (dig3 = dig1 + 1; dig3 <= '9'; dig3++, j++)
 {
-for (dig4 = dig2 + 1; dig4 <= '9'; dig4++)
+for (dig4 = dig2 + 1; dig4 <= '9'; dig4++,j++)
 {
 if (k > 0)
 {
