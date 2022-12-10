ec80a92a363d2b97d2c25534816f349460153270
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-09 19:24:12 -0800

    double digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*int dig1 - first digit
+*dig2 - second digit
+*k - checker integer
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int dig1, dig2, k = 0;
+for (dig1 = '0'; dig1 <= '9'; dig1++)
+{
+for (dig2 = dig1 + 1; dig2 <= '9'; dig2++, k++)
+{
+if (k > 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(dig1);
+putchar(dig2);
+}
+}
+putchar('\n');
+return (0);
+}
