d6cf09aa371b710fcf1895350faa382c38bfdd4d
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-13 18:03:08 -0800

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-/**
 * printPut - 'program to print'
 * printPut(): function to print string
 */
@@ -29,18 +19,4 @@ else
 putchar(msg[count]);
 }
 }
-/**
-* print_alphabet - 'print alphabet in lowercase'
-* print_alphabet(): function call to print alphabet in lowercase
-*
-*/
-void print_alphabet(void)
-{
-int ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-}
-#endif
+int _putchar(char);
