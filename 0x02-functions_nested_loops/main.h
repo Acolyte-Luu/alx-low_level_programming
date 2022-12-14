cddfd1b87e5c9dc35a95f316e3f670b7b648dcf7
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-13 18:25:17 -0800

    update main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
 /**
 * printPut - 'program to print'
 * printPut(): function to print string
 */
-void printPut(void)
-{
-int count;
-int max = 80;
-char msg[] = "_putchar";
-for (count = 0; count < max; count++)
-{
-if (msg[count] == '\0')
-{
-break;
-}
-else
-_putchar(msg[count]);
-}
-}
 int _putchar(char);
 void print_alphabet(void);
-#endif
