766e03df37e9f7513113edbd5242f866ae98ad7b
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-13 20:15:12 -0800

    updated file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 *add - adds up two numbers
-*int n - first integer
-*int m - second integer
+*@n: first integer
+*@m: second integer
 *Return: returns 0
 */
 int add(int n, int m)
