d0924c5da971c7d3a0a14719ae837d31264012a1
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-13 18:10:25 -0800

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,4 +20,13 @@ putchar(msg[count]);
 }
 }
 int _putchar(char);
+void print_alphabet(void)
+{
+int ch;  
+for (ch = 'a'; ch <= 'z'; ch++)    
+{      
+putchar(ch);      
+}  
+putchar('\n');
+}
 #endif
