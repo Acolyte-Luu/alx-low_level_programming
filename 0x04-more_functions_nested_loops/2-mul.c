b08fd25e881b96a60ef9403f922249a4aff4a9a7
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-15 14:56:23 -0800

    function to multiply two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - multiplies two numbers
+*@a: first integer
+*@b: second integer
+*Return: returns 0
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 /**
-* printPut - 'program to print'
-* printPut(): function to print string
+* printPut - 'header file with functions'
+*
 */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +14,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
 void print_to_98(int n);
