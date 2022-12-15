65fb2f5d01948655a7fc3657f8bae0a860f86dbe
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-15 11:45:44 -0800

    Debugging 102

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/**
-*while (i < 10) - commenting out infinte loop
-*{\\o/\n": to print
-*putchar(i);
-*}
-*/
+
+/*while (i < 10) - commenting out infinte loop*/
+/*{  @\\o/\n": to print*/
+/*putchar(i);*/
+/*}*/
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
