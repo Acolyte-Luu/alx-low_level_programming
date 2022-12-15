46bf362bfadc754e745995d00186cb0a4aea8b16
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-15 14:48:16 -0800

    digit checker

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 * printPut(): function to print string
 */
 int _isupper(int c);
-int _putchar(char);
+int _isdigit(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
