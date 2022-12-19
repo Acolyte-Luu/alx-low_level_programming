7052a542155f52a7339411eb16dc43550abb7f98
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-19 13:08:28 -0800

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,8 @@
 /**
 * reset_to_98 - function to update value of variable
 * @n: variable to reset
-* int c - int used
 */
 void reset_to_98(int *n)
 {
-int c;
-int *n;
-n = &c;
-c = 98;
+*n = 98;
 }
