d347caf5f590b1b46132c7337cb9cd11273a9c92
Acolyte-Luu <kwadwoaddo97@gmail.com>
2022-12-19 16:47:32 -0800

    more and more pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* rev_string - reverses string
+* @s: string
+*/
+void rev_string(char *s)
+{
+int count = 0;
+int a;
+int b;
+char *str;
+char temp;
+while (count >= 0)
+{
+if (s[count] == '\0')
+break;
+count = count + 1
+}
+str = s;
+for (a = 0; a < (count - 1); a++)
+{
+for (b = a + 1; b > 0; b--)
+{
+temp = *(str + b);
+(str + b) = (str + (b - 1));
+*(str + (b - 1)) = temp;
+}
+}
+}
