3a2a1f53932075d847219c0782e15a794359cdb3
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-01-05 16:18:09 -0800

    test

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,16 +17,16 @@ return (is_prime_checker(n, 2));
 int is_prime_checker(int n, int i)
 {
 if (n <= 1)
-{
-return (0);
-}
-if (i > _sqrt_recursion(n))
-{
-return (1);
-}
+    {
+      return (0);
+    }
+if (i > sqrt(n))
+    {
+      return (1);
+    }
 if (n % i == 0)
-{
-return (0);
-}
-return (is_prime_checker(n, i + 1));
+    {
+      return (0);
+    }
+  return (is_prime_checker(n, 2));
 }
