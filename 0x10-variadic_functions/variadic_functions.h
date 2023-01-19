c664d8834a57d5e3d1256cf0e58564950efc750d
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-01-19 11:49:05 -0800

    fixing betty issues

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,5 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-typedef struct validTypes
-{
-char *valid;
-void (*f)();
-} v_types;
+#ifndef VARIADIC_FUNCS
+#define VARIADIC_FUNCS
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
