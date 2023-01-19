fa6fd5bf7eac6a469dab5016ced82dc8445e1584
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-01-19 13:06:14 -0800

    function prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,50 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* print_all - Variadic function that can print any argument passed in
-* as long as it is a character, integer, float, or string
-* @format: The data type being passed into the function
+* print_all - A function prints anything.
+* @format: A list of type of arguments passed to the function.
 */
 void print_all(const char * const format, ...)
 {
+va_list ap;
+char *temp;
 int i = 0;
-va_list argument_input;
-char *str = "(nil)";
-char *sarray = NULL;
-char *separator = ", ";
-va_start(argument_input, format);
-while (format[i] != '\0' && format != NULL)
+va_start(ap, format);
+while (format == NULL)
+{
+printf("\n");
+return;
+}
+while (format[i])
 {
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(argument_input, int));
+printf("%c", (char) va_arg(ap, int));
 break;
 case 'i':
-printf("%d", va_arg(argument_input, int));
+printf("%d", va_arg(ap, int));
 break;
 case 'f':
-printf("%f", va_arg(argument_input, double));
+printf("%f", (float) va_arg(ap, double));
 break;
 case 's':
-sarray = va_arg(argument_input, char *);
-if (sarray == NULL)
-sarray = str;
-printf("%s", sarray);
+temp = va_arg(ap, char*);
+if (temp != NULL)
+{
+printf("%s", temp);
 break;
-default:
-i++;
-continue;
 }
-if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's'))
-printf("%s", separator);
+printf("(nil)");
+break;
+}
+if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+format[i] == 's') && format[(i + 1)] != '\0')
+printf(", ");
 i++;
 }
+va_end(ap);
 printf("\n");
-va_end(argument_input);
 }
