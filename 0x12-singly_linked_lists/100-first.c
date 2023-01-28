11bddaaf535a819fdb4a2d48b1455dd2590ca437
Acolyte-Luu <kwadwoaddo97@gmail.com>
2023-01-27 18:04:56 -0800

    adding projects files and README

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main_constructor(void)__attribute__((constructor));
+/**
+* main_constructor - A function that prints a message before main executed
+*/
+void main_constructor(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+lessons and tasks to teach linked lists
